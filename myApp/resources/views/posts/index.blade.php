9efcb9924a18b26397aea0f3c26225f83b25b234
Jahid1999 <bsse1030@iit.du.ac.bd>
2020-09-16 15:43:29 +0600

    added pagination and routing

--- myApp/resources/views/posts/index.blade.php
+++ myApp/resources/views/posts/index.blade.php
@@ -2,15 +2,16 @@
 
 @section('content')
     <h2>Posts</h2>
-    @if ( count($posts) >1)
+    @if ( count($posts) > 0)
         @foreach ($posts as $post)
             <div class="well"> 
-                <h4>{{$post->title}} </h4>
+                <h4> <a href="/posts/{{$post->id}}"> {{$post->title}} </a> </h4>
                 <small> Posted on {{$post->created_at}} </small>
 
             </div>
             
         @endforeach
+        {{$posts->links()}}
 
     @else
         <p> No posts found!!</p>
