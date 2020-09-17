6e4235efea2ee6efa8a51918a066cef450ccde03
Jahid1999 <bsse1030@iit.du.ac.bd>
2020-09-17 23:25:26 +0600

    added post edit and delete option

--- myApp/app/Http/Controllers/PostsController.php
+++ myApp/app/Http/Controllers/PostsController.php
@@ -62,7 +62,7 @@ class PostsController extends Controller
      */
     public function show($id)
     {
-        $post =Post::find($id);
+        $post = Post::find($id);
         return view('posts.show')->with('post', $post);
     }
 
@@ -74,7 +74,8 @@ class PostsController extends Controller
      */
     public function edit($id)
     {
-        //
+        $post = Post::find($id);
+        return view('posts.edit')->with('post', $post);
     }
 
     /**
@@ -86,7 +87,17 @@ class PostsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $this->validate($request, [
+            'title' => 'required',
+            'body' => 'required'
+        ]);
+        //create post
+        $post = Post::find($id);
+        $post->title = $request->input('title');
+        $post->body = $request->input('body');
+        $post->save();
+
+        return redirect('/posts')->with('success', 'Post Updated.');
     }
 
     /**
@@ -97,6 +108,9 @@ class PostsController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $post = Post::find($id);
+        $post->delete();
+        return redirect('/posts')->with('success', 'Post Deleted.');
+
     }
 }
